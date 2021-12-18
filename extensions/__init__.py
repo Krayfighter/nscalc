3b8a16e23afa1d8bd42ac1d121f0634992183194
Aiden Kring <aidenjkring@gmail.com>
2021-12-17 23:03:16 -0500

    import all extensions by default

--- extensions/__init__.py
+++ extensions/__init__.py
@@ -1,1 +1,8 @@
-from extensions.examples import *
+from importlib import import_module
+from os import getcwd, listdir
+
+
+for item in listdir(getcwd()+'/extensions'):
+    if item.endswith('.py') and item != '__init__.py':
+        globals()[item[:-3]] = import_module('extensions.'+item[:-3])
+
