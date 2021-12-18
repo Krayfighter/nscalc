c0371f7203975d42f1816817d7e79fed12b44c65
Aiden Kring <aidenjkring@gmail.com>
2021-12-17 23:39:27 -0500

    fix programatic imports to be easier to use

--- extensions/__init__.py
+++ extensions/__init__.py
@@ -2,7 +2,13 @@ from importlib import import_module
 from os import getcwd, listdir
 
 
+# !!!!!   The stack overflow guy said that this was dangerous
 for item in listdir(getcwd()+'/extensions'):
     if item.endswith('.py') and item != '__init__.py':
-        globals()[item[:-3]] = import_module('extensions.'+item[:-3])
-
+        item = item[:-3]
+        module = import_module('extensions.'+item).__dict__
+        try:
+            to_import = module.__all__
+        except AttributeError:
+            to_import = [name for name in module if not name.startswith('_')]
+        globals().update({name: module[name] for name in to_import})\
